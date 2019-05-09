c9d1727a41c1b4a91f6893d58270370d52a97e1b
eamorgado <edu.dado99@gmail.com>
2019-05-09 22:08:22 +0100

    Added Prediction files

--- Version1/DataSet.py
+++ Version1/DataSet.py
@@ -7,8 +7,10 @@ class DataSet():
         self.raw_inputs = [[int(r) for r in row] for row in csv_reader]
     def getRaw(self):
         return self.raw_inputs
-    def getInputs(self):
-        return [row[:-1]for row in self.getRaw()]
+    def getInputs(self,test=False):
+        if not test:
+            return [row[:-1]for row in self.getRaw()]
+        return self.getRaw()
     def getTargets(self):
         return [[x[-1]] for x in self.raw_inputs]
     def getNumInputElem(self):
--- Version1/Main.py
+++ Version1/Main.py
@@ -1,51 +1,97 @@
 import matplotlib as plt
 import pprint as pp
+from prettytable import PrettyTable
 import numpy as np
+from math import ceil
 
 import DataSet as DS
 import NeuralNetwork as NN
 
 
-filename = 'parity4.txt'
-data = DS.DataSet(filename)
+'''-----------------------------------------------------------------------------
+Open and read a File with data 
+-----------------------------------------------------------------------------'''
+def openDataSet(filename,v):
+    data = DS.DataSet(filename)
+    n = data.getNumInputElem()
+    inputs = data.getInputs(test=v)
+    targets = data.getTargets()
+    return n,inputs,targets
 
-n = data.getNumInputElem()
-
-net = NN.NeuralNetwork(n,n,1)
-inputs = data.getInputs()
-targets = data.getTargets()
-
-'''
-pp.pprint(inputs)
-pp.pprint(targets)
-'''
-print("-----Neural Networks with "+str(n)+"Input and hidden units and 1 output unit-----\n")
-print("Testing the same Neural Network with increasing values for the learning reate:")
-for lr in np.arange(0.05,0.55,0.05):
-    for epc in range(1000):
-        error = []
-        for x,y in zip(inputs,targets):
-            error.append(abs(max(net.train(x,y,lr),key=abs)))
-        if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
-            break
-    print("Training with learning rate = "+str(lr)+" Epochs:"+str(epc+1))
-
-entry = [0,1,1,1]
-guess = net.predict(entry)
-#entry = [1,0,0,0,1,1,1,1,1,0,0]
-#guess = net.predict(entry)
-print("\nGuessing "+str(entry)+": "+str(guess))
-print()
-
-print("Training diferent neural networks for diferent learning rates:")
-for version,lr in enumerate(np.arange(0.05,0.55,0.05)):
+def makePrediction(network,guessfilename):
+    _,entries,_ = openDataSet(guessfilename,True)
+    predictions = []
+    for entry in entries:
+        prediction = network.predict(entry)
+        predictions.append(prediction)
+    return entries,predictions
+'''-----------------------------------------------------------------------------
+Train and test the same Neural Network for different Learning Rates
+-----------------------------------------------------------------------------'''
+def trainSame(n,inputs,targets,guessfilename):
+    print("-----Neural Networks with "+str(n)+"Input and hidden units and 1 output unit-----\n")
     net = NN.NeuralNetwork(n,n,1)
-    for epc in range(1000):
-        error = []
-        for x,y in zip(inputs,targets):
-            error.append(abs(max(net.train(x,y,lr),key=abs)))
-        if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
-            break
-    print("Network:"+str(version)+"--Training with learning rate = "+str(lr)+" Epochs:"+str(epc+1))
 
-print("-----------------------------------------------------------------------------------")
+    tb = PrettyTable()
+    tb.title = "Same Network new Learning Rates"
+    tb.field_names = ["Learning Rate","Epochs"]
+    for lr in np.arange(0.05,0.55,0.05):
+        for epc in range(1000):
+            error = []
+            for x,y in zip(inputs,targets):
+                error.append(abs(max(net.train(x,y,lr),key=abs)))
+            if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
+                break
+        tb.add_row([str(ceil(lr*100.0)/100.0),str(epc+1)])
+    print(tb)
+
+    entries,predictions = makePrediction(net,guessfilename)
+    tb = PrettyTable()
+    tb.title = "Guessing Output"
+    tb.field_names = ["Entry","Prediction"]
+    for entry,prediction in zip(entries,predictions):        
+        tb.add_row([str(entry),str(prediction)])
+    print(tb)
+    print("-----------------------------------------------------------------------------------")
+
+
+'''-----------------------------------------------------------------------------
+Train and test different Neural Networks for different Learning Rates
+-----------------------------------------------------------------------------'''
+def trainDifferent(n,inputs,targets,guessfilename):
+    print("-----Neural Networks with "+str(n)+"Input and hidden units and 1 output unit-----\n")
+    net = NN.NeuralNetwork(n,n,1)
+
+    tb = PrettyTable()
+    tb.title = "Diferent Networks new Learning Rates"
+    tb.field_names = ["Network","Learning Rate","Epochs"]
+
+    tg = PrettyTable()
+    tg.title = "Guessing Output for every Network"
+    tg.field_names = ["Network Id","Entry","Prediction"]
+
+    for version,lr in enumerate(np.arange(0.05,0.55,0.05)):
+        net = NN.NeuralNetwork(n,n,1)
+        for epc in range(1000):
+            error = []
+            for x,y in zip(inputs,targets):
+                error.append(abs(max(net.train(x,y,lr),key=abs)))
+            if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
+                break
+        tb.add_row([str(version),str(ceil(lr*100.0)/100.0),str(epc+1)])
+
+        entries,predictions = makePrediction(net,guessfilename)
+        
+        for entry,prediction in zip(entries,predictions):        
+            tg.add_row([str(version),str(entry),str(prediction)])
+
+    print(tb)
+    print()
+    print(tg)
+    print("-----------------------------------------------------------------------------------")
+
+#-------------------------------------------------------------------------------
+filename = "parity11.txt"
+guessfilename = "parity11_predictions.txt"
+n,inputs,targets = openDataSet(filename,False)
+trainSame(n,inputs,targets,guessfilename)
