648793171f743796e28f5901ea62364fae834b74
eamorgado <edu.dado99@gmail.com>
2019-05-09 21:02:28 +0100

    Neural Networks

--- Version1/Main.py
+++ Version1/Main.py
@@ -19,31 +19,33 @@ targets = data.getTargets()
 pp.pprint(inputs)
 pp.pprint(targets)
 '''
-
+print("-----Neural Networks with "+str(n)+"Input and hidden units and 1 output unit-----\n")
+print("Testing the same Neural Network with increasing values for the learning reate:")
 for lr in np.arange(0.05,0.55,0.05):
-    for epc in range(6000):
+    for epc in range(1000):
         error = []
         for x,y in zip(inputs,targets):
             error.append(abs(max(net.train(x,y,lr),key=abs)))
-        #print(error)
         if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
-            #print("LR:"+str(lr)+", Epoch:"+str(epc))
             break
     print("Training with learning rate = "+str(lr)+" Epochs:"+str(epc+1))
-    
-_,_,_,guess = net.feedFoward([0,1,1,1])
-#_,_,_,guess = net.feedFoward([1,0,0,0,1,1,1,1,1,0,0])
-print(guess)
 
+entry = [0,1,1,1]
+_,_,_,guess = net.feedFoward(entry)
+#entry = [1,0,0,0,1,1,1,1,1,0,0]
+#_,_,_,guess = net.feedFoward(entry)
+print("\nGuessing "+str(entry)+":\n"+str(guess))
+print("\n")
 
-for lr in np.arange(0.05,0.55,0.05):
+print("Training diferent neural networks for diferent learning rates:")
+for version,lr in enumerate(np.arange(0.05,0.55,0.05)):
     net = NN.NeuralNetwork(n,n,1)
-    for epc in range(6000):
+    for epc in range(1000):
         error = []
         for x,y in zip(inputs,targets):
             error.append(abs(max(net.train(x,y,lr),key=abs)))
-        #print(error)
         if sum(i <= 0.05 for i in error)>=(2*len(error))/3:
-            #print("LR:"+str(lr)+", Epoch:"+str(epc))
             break
-    print("Training with learning rate = "+str(lr)+" Epochs:"+str(epc+1))
+    print("Network:"+str(version)+"--Training with learning rate = "+str(lr)+" Epochs:"+str(epc+1))
+
+print("-----------------------------------------------------------------------------------")
